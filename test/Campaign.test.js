93e481d7e5e9f12ec1cda603de0c550dc567eef2
davis_chang <davis chang>
2018-07-07 11:00:22 +0800

    add few test for checking manager and approvers

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
+
 const web3 = new Web3(ganache.provider());
 
 const compiledFactory = require('../ethereum/build/CampaignFactory.json');
@@ -19,14 +20,14 @@ beforeEach(async () => {
 
   await factory.methods.createCampaign('100').send({
     from: accounts[0],
-    gas: '1000000'
+    gas: '1000000',
   });
 
   [campaignAddress] = await factory.methods.getDeployedCampaigns().call();
 
   campaign = await new web3.eth.Contract(
     JSON.parse(compiledCampaign.interface),
-    campaignAddress
+    campaignAddress,
   );
 });
 
@@ -34,5 +35,20 @@ describe('Campaigns', () => {
   it('deploys a factory and a camppaign', () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
+  });
+
+  it('marks caller as the campaign manager', async () => {
+    const manager = await campaign.methods.manager().call();
+    assert.equal(accounts[0], manager);
+  });
+
+  it('allow people to contribute momey and marks them as approvers', async () => {
+    await campaign.methods.contribute().send({
+      value: '200',
+      from: accounts[1],
+    });
+
+    const isContributor = await campaign.methods.approvers(accounts[1]).call();
+    assert(isContributor);
   });
 });
