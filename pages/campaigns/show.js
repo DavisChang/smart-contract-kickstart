1acd57f6ec1f65fb1beaabbe41bf613a9992b1a0
davis_chang <davis chang>
2018-08-01 14:45:58 +0800

    get campaign data

--- pages/campaigns/show.js
+++ pages/campaigns/show.js
@@ -1,7 +1,22 @@
 import React, { Component } from 'react';
 import Layout from '../../components/Layout';
+import Campaign from '../../ethereum/campaign';
 
 class CampaignShow extends Component {
+  static async getInitialProps(props) {
+    const { query } = props;
+    const campaign = Campaign(query.address);
+    const summary = await campaign.methods.getSummary().call();
+
+    return {
+      minimumContribution: summary[0],
+      balance: summary[1],
+      requestsCount: summary[2],
+      approversCount: summary[3],
+      manager: summary[4],
+    };
+  }
+
   render() {
     return (
       <Layout>
