1539b56f28879c96867ca120cc9f1332c44a5d8e
davis_chang <davis chang>
2018-08-03 21:59:38 +0800

    campaign request form

---
+++ components/RequestRow.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Table } from 'semantic-ui-react';
+import web3 from '../ethereum/web3';
+
+class RequestRow extends Component {
+
+  render() {
+    const { Row, Cell } = Table;
+    const { id, request, approversCount } = this.props;
+
+    return (
+      <Row>
+        <Cell>{id}</Cell>
+        <Cell>{request.description}</Cell>
+        <Cell>{web3.utils.fromWei(request.value)}</Cell>
+        <Cell>{request.recipient}</Cell>
+        <Cell>{request.approvalCount}/{approversCount}</Cell>
+        <Cell>Approve action</Cell>
+        <Cell>Finalize action</Cell>
+      </Row>
+    );
+  }
+}
+
+export default RequestRow;
