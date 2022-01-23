5cef60874b30c45b0c5648d925ff2bccda95b87f
Davis Chang <davis.chang@langlive.com>
2022-01-23 23:10:24 +0800

    Deal with web3 on browser and server side

--- campaign-fe/utils/ethereum/web3.js
+++ campaign-fe/utils/ethereum/web3.js
@@ -1,7 +1,18 @@
 import Web3 from 'web3'
  
-window.ethereum.request({ method: "eth_requestAccounts" })
+let web3;
  
-const web3 = new Web3(window.ethereum)
+if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
+  // We are in the browser and metamask is running.
+  window.ethereum.request({ method: "eth_requestAccounts" })
+  web3 = new Web3(window.ethereum)
+} else {
+  // We are on the server *OR* the user is not running metamask.
+  // Connect to Rinkebt test network by infura.
+  const provider = new Web3.providers.HttpProvider(
+    "https://rinkeby.infura.io/v3/9428e1df1bdd468e9c748fdf057ebbf9"
+  );
+  web3 = new Web3(provider)
+}
  
 export default web3
