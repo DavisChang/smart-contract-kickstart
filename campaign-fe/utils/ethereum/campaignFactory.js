b5f8644eecc385e2a2cfce7b6d6f9e71a872df85
Davis Chang <davis.chang@langlive.com>
2022-01-29 22:05:44 +0800

    1. Update Contracts 2. Redeployed CampaignFactory

--- campaign-fe/utils/ethereum/campaignFactory.js
+++ campaign-fe/utils/ethereum/campaignFactory.js
@@ -2,7 +2,7 @@ import web3 from './web3'
 import CampaignFactory from './build/CampaignFactory.json'
 
 // Address
-const CONTRACT_ADDRESS = '0x851539f3CB937CDE00029B803ff22F95ACFF39B7'
+const CONTRACT_ADDRESS = '0x3DA576b66B77a01d783e2926057495dBe29Cb2f9'
 
 const instance = new web3.eth.Contract(
   CampaignFactory.abi,
