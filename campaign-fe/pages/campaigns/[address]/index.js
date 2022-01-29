d5de4ecf906a5c95a8ffecfd6a388ef87db35d3b
Davis Chang <davis.chang@langlive.com>
2022-01-29 22:38:48 +0800

    1. Get campaign summary data 2. Add campaign util

--- campaign-fe/pages/campaigns/[address]/index.js
+++ campaign-fe/pages/campaigns/[address]/index.js
@@ -2,11 +2,25 @@ import { useRouter } from 'next/router'
 import { Container, Header } from 'semantic-ui-react'
 import HeadComps from 'components/Head/HeadComps'
 import Layout from 'components/Layout/Layout'
+import Campaign from 'utils/ethereum/campaign'
 
-export default function Address() {
+export default function Address({
+  minimumContribution,
+  balance,
+  requestsCount,
+  approversCount,
+  managerAddress
+}) {
   const router = useRouter()
   const { address } = router.query
-  
+  console.log({
+    address,
+    minimumContribution,
+    balance,
+    requestsCount,
+    approversCount,
+    managerAddress
+  })
   return (
     <Layout>
       <HeadComps title="Address"/>
@@ -15,4 +29,27 @@ export default function Address() {
       </Container>
     </Layout>
   )
+}
+
+export async function getServerSideProps(context) {
+  try {
+    const { address } = context.params
+    const campaign = Campaign(address)
+    const summary = await campaign.methods.getSummary().call()
+    return {
+      props: {
+        minimumContribution: summary[0],
+        balance: summary[1],
+        requestsCount: summary[2],
+        approversCount: summary[3],
+        managerAddress: summary[4],
+      }
+    }
+  } catch (err) {
+    console.log('error message:', err.message)
+  }
+
+  return {
+    props: {}
+  }
 }
