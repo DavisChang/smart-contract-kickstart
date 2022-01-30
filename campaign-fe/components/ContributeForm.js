803938c42e1f311444dea6952d03c05329ebadbf
Davis Chang <davis.chang@langlive.com>
2022-01-30 17:43:23 +0800

    Contribute a campaign

---
+++ campaign-fe/components/ContributeForm.js
@@ -0,0 +1,61 @@
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { Form, Input, Button, Message } from 'semantic-ui-react'
+import Campaign from 'utils/ethereum/campaign'
+import web3 from 'utils/ethereum/web3'
+
+const ContributeForm = ({ address }) => {
+  const router = useRouter()
+  const [value, setValue] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [errorMag, setErrorMag] = useState('')
+
+  const handleOnChange = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault()
+    setLoading(true)
+    setErrorMag('')
+    try {
+      const campaign = Campaign(address)
+      console.log(campaign)
+      const accounts = await web3.eth.getAccounts()
+      await campaign.methods.contribute().send({
+        from: accounts[0],
+        value: web3.utils.toWei(value, 'ether')
+      })
+      afterSubmit()
+    } catch(err) {
+      console.log(err)
+      setErrorMag(err.message)
+    } finally {
+      setLoading(false)
+      setValue(0)
+    }
+  }
+
+  const afterSubmit = () => {
+    router.replace(`/campaigns/${address}`)
+  }
+  return (
+    <Form onSubmit={handleOnSubmit} loading={loading} error={!!errorMag}>
+      <Form.Field>
+        <label>Amount to Contribute</label>
+        <Input
+          type="number"
+          label="ether"
+          labelPosition="right"
+          placeholder="Amount to Contribute"
+          value={value}
+          onChange={handleOnChange}
+        />
+      </Form.Field>
+      <Message error header="Oops!" content={errorMag} />
+      <Button loading={loading} primary type='submit'>Contribute!</Button>
+    </Form>
+  )
+}
+
+export default ContributeForm
--- campaign-fe/pages/campaigns/[address]/index.js
+++ campaign-fe/pages/campaigns/[address]/index.js
@@ -1,18 +1,18 @@
-import { useRouter } from 'next/router'
-import { Container, Header } from 'semantic-ui-react'
+import { Container, Header, Card, Grid } from 'semantic-ui-react'
 import HeadComps from 'components/Head/HeadComps'
 import Layout from 'components/Layout/Layout'
 import Campaign from 'utils/ethereum/campaign'
+import web3 from 'utils/ethereum/web3'
+import ContributeForm from 'components/ContributeForm'
 
 export default function Address({
+  address,
   minimumContribution,
   balance,
   requestsCount,
   approversCount,
   managerAddress
 }) {
-  const router = useRouter()
-  const { address } = router.query
   console.log({
     address,
     minimumContribution,
@@ -21,11 +21,61 @@ export default function Address({
     approversCount,
     managerAddress
   })
+
+  const renderCards = () => {
+    const items = [
+      {
+        header: managerAddress,
+        meta: 'Address of manager',
+        description:
+          'The manager created this campaign and can create requests to withdrew money',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: minimumContribution,
+        meta: 'Minimum contribution (wei)',
+        description:
+          'You must contribute at least this much wei to become an approver',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: requestsCount,
+        meta: 'Number of requests',
+        description:
+          'A request tries to withdraw money from the contract. Requests must be approved by approvers',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: approversCount,
+        meta: 'Number of approvers',
+        description:
+          'Number of people who have already donated to this campaign',
+        style: { overflowWrap: 'break-word' }
+      },
+      {
+        header: web3.utils.fromWei(balance, 'ether'),
+        meta: 'Campaign balance (ether)',
+        description:
+          'The balance is how much money this campaign has left to spand',
+        style: { overflowWrap: 'break-word' }
+      },
+    ]
+
+    return <Card.Group items={items} />
+  }
   return (
     <Layout>
       <HeadComps title="Address"/>
       <Container>
         <Header as='h3'>Campaign Details</Header>
+        <Grid>
+          <Grid.Column width={10}>
+            {renderCards()}
+          </Grid.Column>
+          <Grid.Column width={6}>
+              <ContributeForm address={address} />
+          </Grid.Column>
+        </Grid>
       </Container>
     </Layout>
   )
@@ -38,6 +88,7 @@ export async function getServerSideProps(context) {
     const summary = await campaign.methods.getSummary().call()
     return {
       props: {
+        address,
         minimumContribution: summary[0],
         balance: summary[1],
         requestsCount: summary[2],
