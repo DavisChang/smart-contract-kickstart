33caf2c04f090ac19ce6904581f937792de46f94
Davis Chang <davis.chang@langlive.com>
2022-02-03 11:37:54 +0800

    get requests one by one, since Solidity not support array of struct

--- campaign-fe/pages/campaigns/[address]/requests.js
+++ campaign-fe/pages/campaigns/[address]/requests.js
@@ -2,8 +2,10 @@ import Link from 'next/link'
 import { Container, Header, Button } from 'semantic-ui-react'
 import HeadComps from 'components/Head/HeadComps'
 import Layout from 'components/Layout/Layout'
+import Campaign from 'utils/ethereum/campaign'
+import web3 from 'utils/ethereum/web3'
 
-export default function Requests({ address }) {  
+export default function Requests({ address, requestsCount, requests }) {  
   return (
     <Layout>
       <HeadComps title="requests"/>
@@ -20,9 +22,27 @@ export default function Requests({ address }) {
 export async function getServerSideProps(context) {
   try {
     const { address } = context.params
+    const campaign = Campaign(address)
+    const requestsCount = await campaign.methods.getRequestsCount().call()
+
+    /*
+    * get requests one by one
+    * fetch all different requests, but it is not yet supported in Solidity
+    * fetch total number of request, retrieve all them one by one
+    */
+    const requests = await Promise.all(
+      Array(Number(requestsCount)).fill().map((ele, index) => {
+        return campaign.methods.requests(index).call()
+      })
+    )
+
+    console.log({ requestsCount, requests })
+
     return {
       props: {
         address,
+        requestsCount,
+        requests,
       }
     }
   } catch (err) {
